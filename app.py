f70a5a1c46989fa745cbd95d5cd6126d89acb91b
Barath <tbarathkalyan@gmail.com>
2025-03-08 11:17:46 +0530

    backend

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 import mysql.connector
 from config import DB_CONFIG
+from routes.auth import auth
 
 app= Flask(__name__)
+app.register_blueprint(auth,url_prefix="/auth")
+
+if __name__== "__main__":
+    app.run(debug=True)
 
 db=mysql.connector.connect(**DB_CONFIG)
 cursor=db.cursor()
