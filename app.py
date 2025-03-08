6ba28869c1b6bddfcf64142a481bf3a826c2886d
Barath <tbarathkalyan@gmail.com>
2025-03-08 11:29:37 +0530

    fixed circular import issue

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
 import mysql.connector
 from config import DB_CONFIG
-from routes.auth import auth
+
 
 app= Flask(__name__)
+
+#DB CONNECTION
+db=mysql.connector.connect(**DB_CONFIG)
+cursor=db.cursor()
+
+#IMPORTING AUTH
+from routes.auth import auth
 app.register_blueprint(auth,url_prefix="/auth")
 
 if __name__== "__main__":
     app.run(debug=True)
 
-db=mysql.connector.connect(**DB_CONFIG)
-cursor=db.cursor()
