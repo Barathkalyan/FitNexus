b3f85afa5379598eccccf85327602b8ae58292cd
Barath <tbarathkalyan@gmail.com>
2025-04-10 08:13:54 +0530

    ajax handling

--- static/js/profile.js
+++ static/js/profile.js
@@ -41,3 +41,34 @@ function prevStep() {
 document.addEventListener("DOMContentLoaded", () => {
   showStep(currentStep);
 });
+document.getElementById("profileForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const data = {
+    age: document.getElementById("age").value,
+    gender: document.getElementById("gender").value,
+    height: document.querySelector("input[name='height']").value,
+    weight: document.querySelector("input[name='weight']").value,
+    fitness_goal: document.getElementById("goal").value,
+    target_weight: document.getElementById("target").value,
+    diet_preference: document.getElementById("diet").value,
+    workout_time: document.querySelector("input[name='workout_time']").value,
+    workout_days: document.querySelector("input[name='workout_days']").value,
+  };
+
+  fetch("/auth/complete_profile", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(res => {
+    if (res.redirect) {
+      window.location.href = res.redirect;
+    } else {
+      alert(res.message || res.error || "Something went wrong!");
+    }
+  })
+  .catch(err => console.error("Error:", err));
+});
+
